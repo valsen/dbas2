53a7ec0630bddb22d182da29b4bdf0b2cb3e7b00
valsen <valsen@users.noreply.github.com>
2018-11-09 08:11:23 +0100

    cleanup

--- NetHub/Models/Movie.cs
+++ NetHub/Models/Movie.cs
@@ -11,16 +11,6 @@ namespace NetHub.Models {
         public int RatingID { get; set; }
         public int Year { get; set; }
 
-        // public string Title { get; set; }
-        // public int Year { get; set; }
-        // public string Description { get; set; }
-        // public string ImgPath { get; set; }
-        // public int RatingID { get; set; }
-
-        // public ICollection<MovieGenre> MoviesGenres { get; set; }
-        // public ICollection<MovieLanguage> MoviesLanguages { get; set; }
-        // public ICollection<MovieDirector> MoviesDirectors { get; set; }
-        // public ICollection<MovieActor> MoviesActors { get; set; }
         public Medium Medium { get; set; }
         public Rating Rating { get; set; }
         public ICollection<MovieActor> MoviesActors { get; set; }
